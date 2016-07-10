7ad59a396f8c89c1b6b861885cdd7f59fcb8c35b
spskauynnda <1033515355@qq.com>
2016-07-10 22:08:53 +0800

    完成简易查询和简易下拉表框功能。轮播图未完成 提交版本：v2.1 提交者：spskauynnda

--- js/react.js
+++ js/react.js
@@ -1,3 +1,31 @@
+var _$ = function(id){
+    if (document.getElementById(id)) {
+        return document.getElementById(id);
+    } else {
+        return false;
+    }
+}
+
+var find = function(){
+    _$("ifr").style.display = "none";
+    _$("btn").onclick=function(){
+        _$("return").style.display = _$("ifr").style.display = "block";
+        _$("name").style.display=_$("no").style.display=_$("btn").style.display="none";
+        _$("ifr").src = "http://m.kuaidi100.com/index_all.html?type="+_$('name').options[_$('name').selectedIndex].value+"&postid="+_$("no").value;
+    }
+}
+
+var ret = function(){
+    _$("return").onclick = function(){
+        _$("return").style.display = _$("ifr").style.display = "none";
+        _$("name").style.display=_$("no").style.display=_$("btn").style.display="block";
+    }
+}
+
+var list = function(){
+
+}
+
 window.onload=function(){
-    alert(device-width);
+    find();     ret();
 }
